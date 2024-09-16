016b822ee8a0f1c6bf7219329001706246da0ba9
jonesjacklewis <jones.jack.lewis@gmail.com>
2024-09-16 20:24:18 +0100

    Refactored hello world

--- 01_go_fundamentals/02_hello_world/hello_test.go
+++ 01_go_fundamentals/02_hello_world/hello_test.go
@@ -3,8 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jack")
-	want := "Hello, Jack"
+	t.Run("Saying hello with a name", func(t *testing.T) {
+		got := Hello("Jack", "")
+		want := "Hello, Jack"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is passed in", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("Expected %q got %q.", want, got)
