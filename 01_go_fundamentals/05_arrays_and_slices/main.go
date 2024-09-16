5c933c9785bf2a291347337b988c6ba45d5bcbd8
jonesjacklewis <jones.jack.lewis@gmail.com>
2024-09-16 23:20:59 +0100

    More tests around slices

--- 01_go_fundamentals/05_arrays_and_slices/main.go
+++ 01_go_fundamentals/05_arrays_and_slices/main.go
@@ -9,3 +9,30 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, slice := range numbersToSum {
+		sums = append(sums, Sum(slice))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, slice := range numbersToSum {
+		if len(slice) <= 1 {
+			sums = append(sums, 0)
+			continue
+		}
+
+		slice = slice[1:]
+
+		sums = append(sums, Sum(slice))
+	}
+
+	return sums
+}
